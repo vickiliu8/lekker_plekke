2f9dd4e2cc0b33ba81167b4bcd7a773ff5cadee8
vickiliu8 <vickiliu@umich.edu>
2015-07-22 16:42:05 +0200

    first commit, added model

---
+++ lekker_plekke/db/migrate/20150722143855_create_places.rb
@@ -0,0 +1,12 @@
+class CreatePlaces < ActiveRecord::Migration
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.string :description
+      t.integer :rating
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
